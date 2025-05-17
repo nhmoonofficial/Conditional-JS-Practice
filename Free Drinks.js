c3e8f86947c5d373e3009dff71ff24662992d1b0
Nazmul-Moon <nhmoonofficial@gmail.com>
2025-05-17 12:26:11 +0600

    create and complete 1st assignemt for condition

---
+++ Free Drinks.js
@@ -0,0 +1,16 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/ 
+
+const Burger = 500;
+
+
+if (Burger >= 500){
+  console.log('you will get a coke free');
+} else {
+  console.log('you have to pay 30tk for coke');
+}
+
